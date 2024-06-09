1671e0f7253cfb6fcd1103cc735ccf10ad7e3f05
Anupamkush9 <anupam.kush9@gmail.com>
2024-06-09 18:07:22 +0530

    done with configuring multiiple database in Django project

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import PhoneBook
 
-# Register your models here.
+@admin.register(PhoneBook)
+class YourModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'number', 'created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at')
--- config/settings.py
+++ config/settings.py
@@ -74,13 +74,41 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': "postgresql_password",
+        'HOST': "django-phone-book-project-database.<random_string>.ap-south-1.rds.amazonaws.com",
+        'PORT': '5432'
+    },
+    'read_db': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': "postgresql_password",
+        'HOST': "django-phone-book-project-database-read-replica.<random_string>.ap-south-1.rds.amazonaws.com",
+        'PORT': '5432'
+    },    
 }
 
+# >>> from app.models import PhoneBook
+# >>> # accessing data from Primary DB
+# >>> contacts = PhoneBook.objects.all()
+# >>> contacts
+# <QuerySet [<PhoneBook: PhoneBook object (1)>, <PhoneBook: PhoneBook object (2)>, <PhoneBook: PhoneBook object (3)>, <PhoneBook: PhoneBook object (4)>]>
+# >>> # now accessing instances from readdb or Reaplica DB
+# >>> contacts = PhoneBook.objects.using('read_db').all()
+# >>> contacts
+# <QuerySet [<PhoneBook: PhoneBook object (1)>, <PhoneBook: PhoneBook object (2)>, <PhoneBook: PhoneBook object (3)>, <PhoneBook: PhoneBook object (4)>]>
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
